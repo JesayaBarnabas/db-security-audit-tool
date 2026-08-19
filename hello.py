623e10ddef7c34e2290e9b25869aec49aff09fbe
JesayaBarnabas <jesayaj@gmail.com>
2026-08-19 20:55:13 +0200

    variables added

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello, I'm building a database security tool!")
+tool_name = "Database Security Audit Tool"
+version = 1
+print("Welcome to the", tool_name)
+print("version:", version)
+
+admin_name = input("Whats your Name?")
+print("Hello,", admin_name, "-Lets check your database security!")
